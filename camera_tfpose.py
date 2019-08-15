d119a8f894cdab8d36c1eab2f0fabc0cb8acfafc
Mokshith <mvoodarla@gmail.com>
2019-08-15 10:51:22 -0700

    add automatic firebase pushing upon human appearance

--- camera_tfpose.py
+++ camera_tfpose.py
@@ -2,10 +2,14 @@ import os
 import cv2
 from base_camera import BaseCamera
 from pose_model import PoseModel
+from database_pusher import DatabasePusher as db
+import time
+import datetime
 
 class Camera(BaseCamera):
     video_source = 1
     model = PoseModel()
+    db = db()
 
     def __init__(self):
         super(Camera, self).__init__()
@@ -16,10 +20,20 @@ class Camera(BaseCamera):
 
     @staticmethod
     def frames():
-
+        last_num_humans = 0
+        last_time_pushed = 10
         while True:
             # read current frame
             img, num_humans = Camera.model.get_prediction()
+            frame_info = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y") + " Humans: " + str(num_humans)
+            cv2.putText(img, frame_info, (10, img.shape[0]-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
+            
+            if (last_num_humans != num_humans or time.time() - last_time_pushed > 5) and num_humans != 0:
+                Camera.db.push(img)
+                last_time_pushed = time.time()
+
+            last_num_humans = num_humans
             # encode as a jpeg image and return it
             yield cv2.imencode('.jpg', img)[1].tobytes()
--- pose_model.py
+++ pose_model.py
@@ -14,12 +14,12 @@ class PoseModel:
         self.w, self.h = model_wh('432x368')
         self.e = TfPoseEstimator(get_graph_path('mobilenet_thin'), target_size=(216, 184))
         self.cam = cv2.VideoCapture(1)
-        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        #self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+        #self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
     
     def get_prediction(self):
         ret_val, image = self.cam.read()
-        image = cv2.flip(image, 1)
+        #image = cv2.flip(image, 1)
         image_h, image_w = image.shape[:2]
         
         humans = self.e.inference(image, resize_to_default=(self.w > 0 and self.h > 0), upsample_size=4.0)
